901ce9631a7167743cc6a25d9c6edce6e00564ce
jenden <willjenden@gmail.com>
2018-05-26 18:44:15 -0700

    Refactor magic numbers to constants

--- tenpin.py
+++ tenpin.py
@@ -1,5 +1,13 @@
 """Challenge: correctly score a game of ten pin bowling"""
 
+# game constants
+FRAMES_PER_GAME = 10
+PINS_PER_FRAME = 10
+ROLLS_PER_FRAME = 2
+SPARE_BONUS_ROLLS = 1
+STRIKE_BONUS_ROLLS = 2
+
+
 class Game:
 
     def __init__(self, rolls=None):
@@ -28,12 +36,12 @@ class Game:
     def check_game_over(self):
         # complete iff ten frames have scores
         num_frames = len(self.frames)
-        all_frames_complete = all([f.score() is not None for f in self.frames[:10]])
-        if num_frames >= 10 and all_frames_complete:
+        all_frames_complete = all([f.score() is not None for f in self.frames[:FRAMES_PER_GAME]])
+        if num_frames >= FRAMES_PER_GAME and all_frames_complete:
             self.game_over = True
 
     def final_score(self):
-        frame_scores = [frame.score() for frame in self.frames[:10]]
+        frame_scores = [frame.score() for frame in self.frames[:FRAMES_PER_GAME]]
         return sum(frame_scores)
     
     def __repr__(self):
@@ -42,11 +50,11 @@ class Game:
 
 class Frame:
     
-    def __init__(self, frame_number, last=None, next=None):
-        self.number = frame_number
+    def __init__(self, frame_number, last_frame=None, next_frame=None):
+        self.frame_number = frame_number
         self.rolls = []
-        self.last = last
-        self.next = next
+        self.last = last_frame
+        self.next = next_frame
 
     def bowl(self, roll):
         self.validate_bowl(roll)
@@ -55,11 +63,11 @@ class Frame:
     def validate_bowl(self, roll):
         if not isinstance(roll, int):
             raise TypeError('The number of pins knocked down must be an integer')
-        if roll < 0 or roll > 10 - sum(self.rolls):
+        if roll < 0 or roll > PINS_PER_FRAME - sum(self.rolls):
             raise ValueError('Cannot knock down more than 10 or less than 0 pins')
         
     def complete(self):
-        if sum(self.rolls) == 10 or len(self.rolls) == 2:
+        if sum(self.rolls) == PINS_PER_FRAME or len(self.rolls) == ROLLS_PER_FRAME:
             return True
         else:
             return False
@@ -71,28 +79,28 @@ class Frame:
         if self.next:
             return self.next
         else:
-            self.next = Frame(self.number + 1, self)
+            self.next = Frame(self.frame_number + 1, self)
             return self.next
 
     def is_spare(self):
-        if sum(self.rolls) == 10 and len(self.rolls) == 2: return True
+        if sum(self.rolls) == PINS_PER_FRAME and len(self.rolls) == ROLLS_PER_FRAME: return True
 
     def is_strike(self):
-        if sum(self.rolls) == 10 and len(self.rolls) == 1: return True
+        if sum(self.rolls) == PINS_PER_FRAME and len(self.rolls) == 1: return True
 
     def score(self):
         if not self.complete():
             return None
-        if self.number > 10:
+        if self.frame_number > FRAMES_PER_GAME:
             return None
 
         score = sum(self.rolls)
 
         try:
             if self.is_spare():
-                return score + self.next_frame().get_sum_of_next_n_rolls(1)
+                return score + self.next_frame().get_sum_of_next_n_rolls(SPARE_BONUS_ROLLS)
             elif self.is_strike():
-                return score + self.next_frame().get_sum_of_next_n_rolls(2)
+                return score + self.next_frame().get_sum_of_next_n_rolls(STRIKE_BONUS_ROLLS)
         except TypeError:
             return None
         
@@ -117,9 +125,9 @@ class Frame:
         elif self.is_spare():
             roll_string = '{} /'.format(self.rolls[0])
         else:
-            padded_rolls = self.rolls + ['_']*(2-len(self.rolls))
+            padded_rolls = self.rolls + ['_']*(ROLLS_PER_FRAME - len(self.rolls))
             roll_string = '{} {}'.format(*padded_rolls)
 
-        return return_string.format(self.number, roll_string, score_string)
+        return return_string.format(self.frame_number, roll_string, score_string)
 
         
